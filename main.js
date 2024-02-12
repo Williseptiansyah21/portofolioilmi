53e2a4561df88db488c05b72ec3a109539ad67d8
Willi Septiansyah <138219874+Williseptiansyah21@users.noreply.github.com>
2024-02-12 14:46:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Industrial Engineering" , "Data Analyst" , "Project Control"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop:true
+});
